6cd1f1b63e7b230cf6cf059c5ce5e808d301ad3c
Raiyan Marzan <raiyanmarzan.rm@gmail.com>
2023-06-10 11:04:12 +0600

    Updated uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,24 +1,6 @@
 <?php
 
 /**
- * Fired when the plugin is uninstalled.
- *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
  * @link       https://#
  * @since      1.0.0
  *
@@ -29,3 +11,16 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+global $wpdb;
+
+$table_names = [
+	$wpdb->prefix . 'pm_vault_folders',
+	$wpdb->prefix . 'pm_vault_items',
+];
+
+foreach ($table_names as $table_name) {
+	$sql = "DROP TABLE IF EXISTS $table_name;";
+	$wpdb->query($sql);
+}
+flush_rewrite_rules();
